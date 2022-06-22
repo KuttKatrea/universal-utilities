4c8c8f897012aac47fabf63e62a7206cb6b1f9e9
Alan Reyes <kutt@katrea.net>
2022-06-22 16:33:20 -0500

    docker-list-context & mkstproj

---
+++ src/kuu/bin/docker_list_context.py
@@ -0,0 +1,26 @@
+import click
+from subprocess import run
+
+UTF_8 = "utf-8"
+
+DOCKERFILE_CONTENT = """
+FROM busybox
+
+WORKDIR /context/
+COPY . /context/
+
+RUN find /context/
+"""
+
+@click.command()
+def main():
+    run(
+        "docker build -t context-ls -f- .".split(" "),
+        input=DOCKERFILE_CONTENT,
+        encoding=UTF_8,
+        check=True
+        )
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/kuu/bin/mkstproj.py
@@ -0,0 +1,40 @@
+import click
+import os
+import json
+
+from click.exceptions import ClickException
+
+SUBLIME_PROJECT_EXTENSION = ".sublime-project"
+UTF_8 = "utf-8"
+
+
+def get_default_filename():
+    base = os.path.basename(os.getcwd())
+    return f"{base}.local"
+
+
+@click.command()
+@click.argument(
+    "filename",
+    default=get_default_filename,
+    type=click.Path(exists=False, dir_okay=False, resolve_path=True, writable=True),
+)
+def main(filename: str):
+    if not filename.endswith(SUBLIME_PROJECT_EXTENSION):
+        filename = filename + SUBLIME_PROJECT_EXTENSION
+
+    if os.path.exists(filename):
+        raise ClickException(f"{filename} already exists.")
+
+    with open(filename, mode="w", encoding=UTF_8) as fp:
+        json.dump(
+            {"folders": [{"path": "."}], "settings": {"remember_open_files": True}},
+            fp=fp,
+            indent=2,
+        )
+
+    click.secho(f"Created sublime project file: {filename}")
+
+
+if __name__ == "__main__":
+    main()
